51e7a72ec96e88636875d3e980b2afeb0a7c61ad
dark horse <asharma13524@gmail.com>
2025-03-26 21:50:08 -0400

    fmting

--- ledger/query/src/batch.rs
+++ ledger/query/src/batch.rs
@@ -13,77 +13,63 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::HashMap;
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use console::{
-  network::prelude::*,
-  program::StatePath,
-  types::Field,
-  prelude::{Error, Result, anyhow},
+    network::prelude::*,
+    prelude::{Error, Result, anyhow},
+    program::StatePath,
+    types::Field,
 };
 
 use crate::QueryTrait;
 
 #[derive(Clone)]
 pub struct InMemoryStateQuery<N: Network> {
-  state_root: N::StateRoot,
-  block_height: u32,
-  state_paths: Arc<HashMap<Field<N>, StatePath<N>>>,
+    state_root: N::StateRoot,
+    block_height: u32,
+    state_paths: Arc<HashMap<Field<N>, StatePath<N>>>,
 }
 
 impl<N: Network> InMemoryStateQuery<N> {
-  pub fn new(
-    block_height: u32,
-    state_root: N::StateRoot,
-    paths: Vec<StatePath<N>>,
-  ) -> Self {
-    let state_paths = paths
-      .into_iter()
-      .map(|p| (*p.tcm(), p))
-      .collect::<HashMap<_, _>>();
-    Self {
-      block_height,
-      state_root,
-      state_paths: Arc::new(state_paths),
+    pub fn new(block_height: u32, state_root: N::StateRoot, paths: Vec<StatePath<N>>) -> Self {
+        let state_paths = paths.into_iter().map(|p| (*p.tcm(), p)).collect::<HashMap<_, _>>();
+        Self { block_height, state_root, state_paths: Arc::new(state_paths) }
     }
-  }
 }
 
 #[cfg_attr(feature = "async", async_trait::async_trait(?Send))]
 impl<N: Network> QueryTrait<N> for InMemoryStateQuery<N> {
-  fn current_state_root(&self) -> Result<N::StateRoot> {
-    Ok(self.state_root)
-  }
+    fn current_state_root(&self) -> Result<N::StateRoot> {
+        Ok(self.state_root)
+    }
 
-  fn current_block_height(&self) -> Result<u32> {
-    Ok(self.block_height)
-  }
+    fn current_block_height(&self) -> Result<u32> {
+        Ok(self.block_height)
+    }
 
-  fn get_state_path_for_commitment(&self, commitment: &Field<N>) -> Result<StatePath<N>> {
-    self
-      .state_paths
-      .get(commitment)
-      .cloned()
-      .ok_or_else(|| anyhow!("Missing state path for commitment: {commitment}"))
-  }
+    fn get_state_path_for_commitment(&self, commitment: &Field<N>) -> Result<StatePath<N>> {
+        self.state_paths
+            .get(commitment)
+            .cloned()
+            .ok_or_else(|| anyhow!("Missing state path for commitment: {commitment}"))
+    }
 
-  #[cfg(feature = "async")]
-  async fn current_state_root_async(&self) -> Result<N::StateRoot> {
-    Ok(self.state_root)
-  }
+    #[cfg(feature = "async")]
+    async fn current_state_root_async(&self) -> Result<N::StateRoot> {
+        Ok(self.state_root)
+    }
 
-  #[cfg(feature = "async")]
-  async fn current_block_height_async(&self) -> Result<u32> {
-    Ok(self.block_height)
-  }
+    #[cfg(feature = "async")]
+    async fn current_block_height_async(&self) -> Result<u32> {
+        Ok(self.block_height)
+    }
 
-  #[cfg(feature = "async")]
-  async fn get_state_path_for_commitment_async(&self, commitment: &Field<N>) -> Result<StatePath<N>> {
-    self
-      .state_paths
-      .get(commitment)
-      .cloned()
-      .ok_or_else(|| anyhow!("Missing state path for commitment: {commitment}"))
-  }
+    #[cfg(feature = "async")]
+    async fn get_state_path_for_commitment_async(&self, commitment: &Field<N>) -> Result<StatePath<N>> {
+        self.state_paths
+            .get(commitment)
+            .cloned()
+            .ok_or_else(|| anyhow!("Missing state path for commitment: {commitment}"))
+    }
 }
--- ledger/query/src/query.rs
+++ ledger/query/src/query.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{QueryTrait, InMemoryStateQuery};
+use crate::{InMemoryStateQuery, QueryTrait};
 
 use console::{
     network::prelude::*,
