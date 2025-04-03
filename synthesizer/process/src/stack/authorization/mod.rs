d335ef88802ede21329f03968e1a06e30d9bc825
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2025-04-03 12:55:32 -0400

    add authorization deserialization patch

--- synthesizer/process/src/stack/authorization/mod.rs
+++ synthesizer/process/src/stack/authorization/mod.rs
@@ -62,8 +62,11 @@ impl<N: Network> TryFrom<(Vec<Request<N>>, Vec<Transition<N>>)> for Authorizatio
             requests.len(),
             transitions.len()
         );
-        // Ensure the requests and transitions are in order.
-        for (index, (request, transition)) in requests.iter().zip_eq(&transitions).enumerate() {
+        // Move the first request to the back in order to match the transitions,
+        // and ensure the requests and transitions are in order.
+        for (index, (request, transition)) in
+            requests.iter().skip(1).chain(requests.iter().take(1)).zip_eq(&transitions).enumerate()
+        {
             // Ensure the request and transition correspond to one another.
             ensure_request_and_transition_matches(index, request, transition)?;
         }
