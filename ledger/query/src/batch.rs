edee4223285553ab893c4e013400db4a4cc5e46d
dark horse <asharma13524@gmail.com>
2025-04-10 10:39:30 -0400

    update license

--- ledger/query/src/batch.rs
+++ ledger/query/src/batch.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Aleo Network Foundation
+// Copyright 2024-2025 Aleo Network Foundation
 // This file is part of the snarkVM library.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
