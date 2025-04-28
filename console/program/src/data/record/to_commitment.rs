c7712500dcdf76ad658253f347d8f0ec0b9b1b39
dark horse <asharma13524@gmail.com>
2025-04-28 16:35:30 -0400

    remove some more prints

--- console/program/src/data/record/to_commitment.rs
+++ console/program/src/data/record/to_commitment.rs
@@ -18,17 +18,11 @@ use super::*;
 impl<N: Network> Record<N, Plaintext<N>> {
     /// Returns the record commitment.
     pub fn to_commitment(&self, program_id: &ProgramID<N>, record_name: &Identifier<N>) -> Result<Field<N>> {
-        println!("🔍 Generating record commitment:");
-        println!("  - Program ID: {}", program_id);
-        println!("  - Record name: {}", record_name);
-        println!("  - Record: {}", self);
-
         // Construct the input as `(program_id || record_name || record)`.
         let input = to_bits_le![program_id, record_name, self];
 
         // Compute the BHP hash of the program record.
         let commitment = N::hash_bhp1024(&input)?;
-        println!("✨ Generated commitment: {}", commitment);
         Ok(commitment)
     }
 }
@@ -36,7 +30,6 @@ impl<N: Network> Record<N, Plaintext<N>> {
 impl<N: Network> Record<N, Ciphertext<N>> {
     /// Returns the record commitment.
     pub fn to_commitment(&self, _program_id: &ProgramID<N>, _record_name: &Identifier<N>) -> Result<Field<N>> {
-        println!("❌ Attempted to generate commitment for Ciphertext record");
         bail!("Illegal operation: Record::to_commitment() cannot be invoked on the `Ciphertext` variant.")
     }
 }
